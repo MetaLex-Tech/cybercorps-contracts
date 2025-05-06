a598fb3c2b441250535891321b61345f6f47f0f0
merisman <6125373+merisman@users.noreply.github.com>
2025-05-06 17:42:41 -0400

    Update warrant-deploy.s.sol

--- script/warrant-deploy.s.sol
+++ script/warrant-deploy.s.sol
@@ -26,7 +26,7 @@ contract BaseScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_MAIN");
         vm.startBroadcast(deployerPrivateKey);
         
-        bytes32 salt = bytes32(keccak256("MetaLexCyberCorpWarrantTest"));
+        bytes32 salt = bytes32(keccak256("MetaLexCyberCorpWarrantTest3"));
         address stableMainNetEth = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
         address stableArbitrum = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
         address stableBase = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
@@ -96,7 +96,7 @@ contract BaseScript is Script {
         CyberAgreementRegistry(registry).createTemplate(bytes32(uint256(3)), "SAFE+T", "https://ipfs.io/ipfs/bafybeih5wvr7zfw76plnb66teaa66rtgoikhhcqh55oecuoxtuw5c3dooi", globalFieldsSafe, partyFieldsSafe);
 
         auth.updateRole(address(multisig), 200);
-        auth.zeroOwner();
+        //auth.zeroOwner();
 
         console.log("auth: ", address(auth));
         console.log("issuanceManagerFactory: ", address(issuanceManagerFactory));
