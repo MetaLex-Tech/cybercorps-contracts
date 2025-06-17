7ccc2cb03a2f93a2db8b804b16c5e6e8343af6a9
0xPrepop <6125373+merisman@users.noreply.github.com>
2025-06-17 11:55:06 -0400

    template for safe, safe+t script

--- script/template.s.sol
+++ script/template.s.sol
@@ -25,22 +25,56 @@ contract BaseScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_MAIN");
         vm.startBroadcast(deployerPrivateKey);
 
-        address registry = address(CyberAgreementRegistry(0x5c3a3f82Dd9713b25656176053e38Af140804bd6));
+        address registry = address(CyberAgreementRegistry(0xa9E808B8eCBB60Bb19abF026B5b863215BC4c134));
+
+ string[] memory globalFieldsSafeT = new string[](17);
+        globalFieldsSafeT[0] = "purchaseAmount";
+        globalFieldsSafeT[1] = "postMoneyValuationCap";
+        globalFieldsSafeT[2] = "expirationTime";
+        globalFieldsSafeT[3] = "governingJurisdiction";
+        globalFieldsSafeT[4] = "disputeResolution";
+        globalFieldsSafeT[5] = "exercisePriceMethod";
+        globalFieldsSafeT[6] = "exercisePrice";
+        globalFieldsSafeT[7] = "unlockStartTimeType";
+        globalFieldsSafeT[8] = "unlockStartTime";
+        globalFieldsSafeT[9] = "unlockingPeriod";
+        globalFieldsSafeT[10] = "latestExpirationTime";
+        globalFieldsSafeT[11] = "unlockingCliffPeriod";
+        globalFieldsSafeT[12] = "unlockingCliffPercentage";
+        globalFieldsSafeT[13] = "unlockingIntervalType";
+        globalFieldsSafeT[14] = "tokenCalculationMethod";
+        globalFieldsSafeT[15] = "minCompanyReserve";
+        globalFieldsSafeT[16] = "tokenPremiumMultiplier";
+
+
+        string[] memory partyFieldsSafeT = new string[](5);
+        partyFieldsSafeT[0] = "name";
+        partyFieldsSafeT[1] = "evmAddress";
+        partyFieldsSafeT[2] = "contactDetails";
+        partyFieldsSafeT[3] = "investorType";
+        partyFieldsSafeT[4] = "investorJurisdiction";
+
+
 
         string[] memory globalFieldsSafe = new string[](5);
-        globalFieldsSafe[0] = "Purchase Amount";
-        globalFieldsSafe[1] = "Post-Money Valuation Cap";
-        globalFieldsSafe[2] = "Expiration Time";
-        globalFieldsSafe[3] = "Governing Jurisdiction";
-        globalFieldsSafe[4] = "Dispute Resolution";
+        globalFieldsSafe[0] = "purchaseAmount";
+        globalFieldsSafe[1] = "postMoneyValuationCap";
+        globalFieldsSafe[2] = "expirationTime";
+        globalFieldsSafe[3] = "governingJurisdiction";
+        globalFieldsSafe[4] = "disputeResolution";
 
-        string[] memory partyFieldsSafe = new string[](3);
-        partyFieldsSafe[0] = "Name";
-        partyFieldsSafe[1] = "EVM Address";
-        partyFieldsSafe[2] = "Contact";
-        
-        CyberAgreementRegistry(registry).createTemplate(bytes32(uint256(1)), "SAFE", "ipfs.io/ipfs/[cid]", globalFieldsSafe, partyFieldsSafe);
 
+        string[] memory partyFieldsSafe = new string[](5);
+        partyFieldsSafe[0] = "name";
+        partyFieldsSafe[1] = "evmAddress";
+        partyFieldsSafe[2] = "contactDetails";
+        partyFieldsSafe[3] = "investorType";
+        partyFieldsSafe[4] = "investorJurisdiction";
+
+        CyberAgreementRegistry(registry).createTemplate(bytes32(uint256(20)), "MetaLeX cyberSAFE US style Reg D v 1.0", "ipfs://bafybeic242ypthamyr3kxnwk4x7sxj7s6svck4xfu3dzgttvic73lihy6m", globalFieldsSafe, partyFieldsSafe);
+        CyberAgreementRegistry(registry).createTemplate(bytes32(uint256(21)), "MetaLeX cyberSAFE + cyberTokenWarrant a16z US style reg D v 1.0", "ipfs://bafybeieozn5ur3gocmmleoph57oznz6acgzrclwkibyijjxv7nobt5tbxa", globalFieldsSafeT, partyFieldsSafeT);
+        CyberAgreementRegistry(registry).createTemplate(bytes32(uint256(22)), "MetaLeX cyberSAFE UK Cayman style Reg S v 1.0", "ipfs://bafybeigm77lgbd5wptji7hoeeqxedippdlvj4eaykgz5eb2bsw2ncboxcu", globalFieldsSafe, partyFieldsSafe);
+        CyberAgreementRegistry(registry).createTemplate(bytes32(uint256(23)), "MetaLeX cyberSAFE + cyberTokenWarrant a16z non-US Reg S v 1.0", "ipfs://bafybeieoahzrvqk3vggrv6zyljlgkrqn2ls5wgpbgp4w4ylenr2r2ftugm", globalFieldsSafeT, partyFieldsSafeT);
         
      }
 }
