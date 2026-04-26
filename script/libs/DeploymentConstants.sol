f08a780a98cd9652e4db7a9d7c4c54f469f54c88
detoo <daily.detoo@gmail.com>
2026-04-26 16:35:42 -0700

    chore: devops-related comments

--- script/libs/DeploymentConstants.sol
+++ script/libs/DeploymentConstants.sol
@@ -38,10 +38,10 @@ library DeploymentConstants {
                     metalexSafe: 0x68Ab3F79622cBe74C9683aA54D7E1BBdCAE8003C,
                     auth: 0x033012a1eDA6e2E00D12CD37c5b63B9440ef5E01,
                     cyberCorpFactory: 0x51413048f3Dfc4516e95BC8e249341B1D53B6cB2,
-                    issuanceManagerFactory: 0xbbD386D237f3b407E6511A52488850b1Da0cCad2,
+                    issuanceManagerFactory: 0xbbD386D237f3b407E6511A52488850b1Da0cCad2, // different from all other chains
                     cyberCorpSingleFactory: 0xBE0D3D13AA07501beAC9b72dE9e9292E66C7A5C4,
                     dealManagerFactory: 0x3982b078f2ac306219c9540Ebc908360a960C251,
-                    roundManagerFactory: 0x9E2A3a07711Ce4b5A2F4D62a5c8f8B5307Af9C34,
+                    roundManagerFactory: 0x9E2A3a07711Ce4b5A2F4D62a5c8f8B5307Af9C34, // different from all other chains
                     cyberAgreementRegistry: 0xa9E808B8eCBB60Bb19abF026B5b863215BC4c134,
                     uriBuilder: 0x5500c095ea7dE6F8a5E15949e24B80604cc670A3,
                     lexchexAuth: 0xeAdeaD5C4A6747D4959489742c143bCDb95a01c2,
@@ -56,10 +56,10 @@ library DeploymentConstants {
                     metalexSafe: 0x68Ab3F79622cBe74C9683aA54D7E1BBdCAE8003C,
                     auth: 0x033012a1eDA6e2E00D12CD37c5b63B9440ef5E01,
                     cyberCorpFactory: 0x51413048f3Dfc4516e95BC8e249341B1D53B6cB2,
-                    issuanceManagerFactory: 0xD353972D7955F421d94d0eA8c42c88c417F7155A,
+                    issuanceManagerFactory: 0xD353972D7955F421d94d0eA8c42c88c417F7155A, // except base-sepolia
                     cyberCorpSingleFactory: 0xBE0D3D13AA07501beAC9b72dE9e9292E66C7A5C4,
                     dealManagerFactory: 0x3982b078f2ac306219c9540Ebc908360a960C251,
-                    roundManagerFactory: 0xc9d5d0DeDD124f9351E5880469f25AB41869aeb9,
+                    roundManagerFactory: 0xc9d5d0DeDD124f9351E5880469f25AB41869aeb9, // except base-sepolia
                     cyberAgreementRegistry: 0xa9E808B8eCBB60Bb19abF026B5b863215BC4c134,
                     uriBuilder: 0x5500c095ea7dE6F8a5E15949e24B80604cc670A3,
                     lexchexAuth: 0xeAdeaD5C4A6747D4959489742c143bCDb95a01c2,
