ac60c62ada6f9ecac58e73b50c32391d17001cca
detoo <daily.detoo@gmail.com>
2026-04-28 14:02:22 -0700

    test: more real-world test cases for staging

--- test/DeployParentCoFactoryAcceptance.t.sol
+++ test/DeployParentCoFactoryAcceptance.t.sol
@@ -108,7 +108,7 @@ contract DeployParentCoFactoryAcceptanceTest is Test {
             subCorpOfficerPrivKey
         );
 
-        (address subCorp,,,,,, bytes32 retId,) = parentCoFactory.deployCorpContractFor(
+        (address subCorp,,,,,,,) = parentCoFactory.deployCorpContractFor(
             subCorpSalt,
             subCompanyName,
             subCompanyType,
@@ -128,6 +128,52 @@ contract DeployParentCoFactoryAcceptanceTest is Test {
         assertTrue(subCorp != address(0));
         assertTrue(registry.hasSigned(agreementId, parentCoOfficers[0].eoa));
         assertTrue(registry.hasSigned(agreementId, subCorpOfficer));
+    }
+
+    function test_realCalldata() public {
+        // Skip test if not on Base Sepolia
+        if(block.chainid != DeploymentConstants.BASE_SEPOLIA) {
+            console2.log("skipping unsupported chain ID: %d ...", block.chainid);
+            return;
+        }
+
+        // https://sepolia.basescan.org/tx/0xd479c8e723bd1999945477fdb7fe9345f488c248b3e02731c050e442aba5d827
+        vm.rollFork(40820661); // one block before the real tx
+
+        string[] memory globalValues = new string[](8);
+        globalValues[0] = "alice";
+        globalValues[1] = "TestCorp";
+        globalValues[2] = "TestCorp S.P.";
+        globalValues[3] = "Segregated Portfolio of Segregated Portfolio Company";
+        globalValues[4] = "Cayman Islands";
+
+        string[] memory partyValues = new string[](2);
+        partyValues[0] = "alice";
+        partyValues[1] = "123";
+
+        (address subCorp,,,,,,,) = parentCoFactory.deployCorpContractFor({
+            salt: 1777409598760,
+            companyName: "TestCorp S.P.",
+            companyType: "Segregated Portfolio of Segregated Portfolio Company",
+            companyJurisdiction: "Cayman Islands",
+            companyContactDetails: "",
+            defaultDisputeResolution: "",
+            _companyPayable: 0x0000000000000000000000000000000000000000,
+            _officer: CompanyOfficer({
+                eoa: 0xd0c3D2b2D19854036a22aFB386920854A67DFC10,
+                name: "alice",
+                contact: "123",
+                title: "Operator"
+            }),
+            _segCoTemplateId: 0xb6da5c8e53767592c0eeb4c5c0d77eae7e1e2e795190e7237d837b3fbc98ed75,
+            _boardConsentTempateId: 0xc02175e98621a996529fb751b30e0b7a8344ece3b00f46a29c1e904c9da87a46,
+            _globalValues: globalValues,
+            _partyValues: partyValues,
+            signature: hex"cf3cd32652fc7998ac55b7c2f457a39519093d28bd0a42c8773f2755ea0ace7831eeb19883a81d501889402f28924a526212155922498b71ae5a109590705b7d1b",
+            deployer: 0xd0c3D2b2D19854036a22aFB386920854A67DFC10
+        });
+
+        assertTrue(subCorp != address(0));
     }
 
     function _createParentCoSignatureHash(uint256 privKey, string memory name, string memory contact) internal returns (bytes memory) {
