41399f79f960030f777cca077c5e28775594e5fa
detoo <daily.detoo@gmail.com>
2026-05-21 09:56:24 -0700

    test: merge related RoundManager tests and test with live forks

--- test/RoundManagerForkTest.t.sol
+++ test/RoundManagerForkTest.t.sol
@@ -11,6 +11,7 @@ import "../dependencies/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "../dependencies/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
 import {IERC1967} from "../dependencies/openzeppelin-contracts/contracts/interfaces/IERC1967.sol";
 import {ILegacyFactory} from "../script/interfaces/ILegacyFactory.sol";
+import {DeploymentConstants} from "../script/libs/DeploymentConstants.sol";
 import {CyberCorpFactory} from "../src/CyberCorpFactory.sol";
 import {IssuanceManagerFactory} from "../src/IssuanceManagerFactory.sol";
 import {CyberCorpSingleFactory} from "../src/CyberCorpSingleFactory.sol";
@@ -66,7 +67,7 @@ contract RoundManagerFCFSForkTest is Test {
     using RoundManagerStorage for RoundManagerStorage.RoundManagerData;
 
     function setUp() public {
-        vm.createSelectFork("base_sepolia", 38956871);
+        vm.createSelectFork("base_sepolia");
     }
 
     function test_RevertIf_FCFS_SubmitEOI_FailLexChexCondition() public {
@@ -789,5 +790,210 @@ contract RoundManagerFCFSForkTest is Test {
         vm.stopPrank();
 
         assertEq(ILexChex(CyberCorpHelper.LEXCHEX_ADDRESS).balanceOf(inv2), 1, "LexChex SHOULD be minted for whitelisted token");
+    }
+
+    // migrated from BaseSepoliaFactoryFcfsForkTest.t.sol
+    function test_CyberCorpFactory_CreatesFcfsRound_AndInvestorFundsIt() public {
+        DeploymentConstants.CoreDeployment memory net = DeploymentConstants.coreV2(block.chainid);
+        ERC20 stable = ERC20(DeploymentConstants.deps(block.chainid).usdc);
+
+        uint256 RAISE_CAP      = 1_000_000e6;
+        uint256 TICKET         =    25_000e6;
+        uint256 PRICE_PER_UNIT = 10e18;
+        uint256 VALUATION      = 20_000_000e18;
+
+        uint256 FOUNDER_PK  = 0xA11CE;
+        uint256 OFFICER_PK  = 0xB0B;
+        uint256 INVESTOR_PK = 0xC0DE;
+
+        address founder  = vm.addr(FOUNDER_PK);
+        address officer  = vm.addr(OFFICER_PK);
+        address investor = vm.addr(INVESTOR_PK);
+
+        CyberAgreementRegistry registry = CyberAgreementRegistry(net.cyberAgreementRegistry);
+        CyberCorpFactory cyberCorpFactory = CyberCorpFactory(net.cyberCorpFactory);
+        CyberCorpSingleFactory cyberCorpSingleFactory = CyberCorpSingleFactory(cyberCorpFactory.cyberCorpSingleFactory());
+        RoundManagerFactory roundManagerFactory = RoundManagerFactory(cyberCorpFactory.roundManagerFactory());
+
+        bytes32 templateId = bytes32(uint256(5535));
+        _createFcfsTemplate(registry, net.metalexSafe, templateId);
+
+        uint256 salt = uint256(keccak256("BaseSepoliaFactoryFcfsForkTest.corp"));
+        bytes32 corpSalt = keccak256(abi.encodePacked(salt));
+        address predictedCorp = cyberCorpSingleFactory.computeCyberCorpSingleAddress(corpSalt);
+        address predictedRoundManager = roundManagerFactory.computeRoundManagerAddress(corpSalt);
+
+        CompanyOfficer memory companyOfficer = CompanyOfficer({
+            eoa: officer,
+            name: "Fork Officer",
+            contact: "officer@cybercorp.test",
+            title: "CEO"
+        });
+
+        string[] memory legalDetails = new string[](1);
+        legalDetails[0] = "Base Sepolia FCFS SAFE";
+
+        bytes[] memory extensionData = new bytes[](1);
+        extensionData[0] = "";
+
+        string[] memory defaultLegend = new string[](1);
+        defaultLegend[0] = "Legend";
+
+        CyberCertData[] memory certData = new CyberCertData[](1);
+        certData[0] = CyberCertData({
+            name: "SEED SAFE",
+            symbol: "SEEDSAFE",
+            uri: "ipfs://base-sepolia-fcfs-safe",
+            securityClass: SecurityClass.SAFE,
+            securitySeries: SecuritySeries.SeriesSeed,
+            extension: address(0),
+            defaultLegend: defaultLegend
+        });
+
+        string[] memory roundPartyValues = new string[](2);
+        roundPartyValues[0] = companyOfficer.name;
+        roundPartyValues[1] = companyOfficer.title;
+
+        uint256 startTime = block.timestamp - 1;
+        uint256 endTime = block.timestamp + 30 days;
+
+        (bytes memory escrowedSig, ) = CyberCorpHelper.computeEscrowSignature(
+            predictedRoundManager,
+            SecuritySeries.SeriesSeed,
+            RAISE_CAP,
+            TICKET,
+            TICKET,
+            RoundType.FCFS,
+            startTime,
+            endTime,
+            templateId,
+            address(stable),
+            PRICE_PER_UNIT,
+            VALUATION,
+            OFFICER_PK,
+            predictedCorp
+        );
+
+        (
+            address corp,
+            ,
+            ,
+            ,
+            address roundManagerAddr,
+            bytes32 roundId
+        ) = cyberCorpFactory.deployCyberCorpAndCreateRound(
+            salt,
+            SecuritySeries.SeriesSeed,
+            "Base Sepolia FCFS Corp",
+            "Delaware C-Corp",
+            "DE",
+            "founder@cybercorp.test",
+            "Arbitration",
+            founder,
+            companyOfficer,
+            legalDetails,
+            extensionData,
+            certData,
+            templateId,
+            address(stable),
+            PRICE_PER_UNIT,
+            VALUATION,
+            roundPartyValues,
+            escrowedSig,
+            RoundType.FCFS,
+            new address[](0),
+            RAISE_CAP,
+            TICKET,
+            TICKET,
+            startTime,
+            endTime,
+            true,
+            true,
+            false
+        );
+
+        assertEq(corp, predictedCorp, "unexpected corp address");
+        assertEq(roundManagerAddr, predictedRoundManager, "unexpected round manager address");
+
+        RoundManager roundManager = RoundManager(roundManagerAddr);
+        Round memory createdRound = roundManager.getRound(roundId);
+        assertEq(createdRound.paymentToken, address(stable), "wrong payment token");
+        assertEq(uint256(createdRound.roundType), uint256(RoundType.FCFS), "wrong round type");
+        assertEq(createdRound.raiseCap, RAISE_CAP, "wrong raise cap");
+        assertEq(createdRound.raised, 0, "new round should start empty");
+
+        deal(address(stable), investor, TICKET * 4);
+
+        string[] memory globalValues = new string[](1);
+        globalValues[0] = "Base Sepolia";
+
+        string[] memory eoiPartyValues = new string[](2);
+        eoiPartyValues[0] = "Fork Investor";
+        eoiPartyValues[1] = "Individual";
+
+        uint256 eoiSalt = 777;
+        bytes memory eoiSignature = CyberCorpHelper.computeEOISignature(
+            registry,
+            templateId,
+            eoiSalt,
+            globalValues,
+            eoiPartyValues,
+            companyOfficer.eoa,
+            INVESTOR_PK
+        );
+
+        EOI memory eoi = EOI({
+            name: "Fork Investor",
+            investorType: "Individual",
+            jurisdiction: "US",
+            contact: "investor@cybercorp.test",
+            minAmount: TICKET,
+            maxAmount: TICKET,
+            expiry: block.timestamp + 7 days,
+            naturalPerson: true,
+            lexchexDetails: CyberCorpHelper.emptyLex()
+        });
+
+        uint256 investorBalanceBefore = stable.balanceOf(investor);
+
+        vm.startPrank(investor);
+        stable.approve(roundManagerAddr, TICKET);
+        (bytes32 agreementId, ) = roundManager.submitEOI(
+            roundId,
+            eoi,
+            globalValues,
+            eoiPartyValues,
+            eoiSignature,
+            eoiSalt,
+            new address[](0),
+            bytes32(0)
+        );
+        vm.stopPrank();
+
+        Round memory fundedRound = roundManager.getRound(roundId);
+        Escrow memory escrow = roundManager.getEscrowDetails(agreementId);
+
+        assertEq(fundedRound.raised, TICKET, "fcfs submission should raise funds immediately");
+        assertEq(investorBalanceBefore - stable.balanceOf(investor), TICKET, "investor should spend the ticket");
+        assertEq(uint256(escrow.status), uint256(EscrowStatus.FINALIZED), "escrow should finalize");
+        assertGt(escrow.corpAssets.length, 0, "allocation should mint corp-side assets");
+    }
+
+    function _createFcfsTemplate(CyberAgreementRegistry registry, address metalexSafe, bytes32 templateId) internal {
+        string[] memory globalFields = new string[](1);
+        globalFields[0] = "Jurisdiction";
+
+        string[] memory partyFields = new string[](2);
+        partyFields[0] = "Officer Name";
+        partyFields[1] = "Officer Title";
+
+        vm.prank(metalexSafe);
+        registry.createTemplate(
+            templateId,
+            "Base Sepolia FCFS Template",
+            "ipfs://base-sepolia-fcfs-template",
+            globalFields,
+            partyFields
+        );
     }
 }
